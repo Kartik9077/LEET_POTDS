d03bc4efbd136dc18ac14c5850284ccbdc8de074
Kartik9077 <kartiksharma15032005@gmail.com>
2025-06-29 13:42:12 +0530

    new

---  Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++  Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -1,5 +1,5 @@
-class Solution {
-    public:
+#include<bits/stdc++.h>
+        using namespace std;
     const int mod=1e9+7;
         int numSubseq(vector<int>& nums, int target) {
             
@@ -22,4 +22,14 @@ class Solution {
     
             return ans;
         }
-    };
+        
+        int main()
+        {  int n;
+            cin>>n;
+            vector<int>arr(n);
+            for(int i=0;i<n;i++)cin>>arr[i];
+            int target ;
+            cin>>target;
+            int ans=numSubseq(arr,target);
+        return 0;
+        }
---
+++ additive  string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    private:
+        bool backtrack(string& num, int start, vector<long long>& store, int size) {
+            if (start == num.size()) {
+                return size > 2;
+            }
+            long long curr = 0;
+            for (int i = start; i < num.size(); i++) {
+                curr = (curr * 10) + num[i] - '0';
+                if (curr > LLONG_MAX / 10 && i + 1 < num.size()) {
+                    break;
+                }
+                if (size >= 2) {
+                    if (store[size - 1] + store[size - 2] == curr) {
+                        store.push_back(curr);
+                        if (backtrack(num, i + 1, store, size + 1)) {
+                            return true;
+                        }
+                        store.pop_back();
+                    } else if (store[size - 1] + store[size - 2] < curr) {
+                        break;
+                    }
+                } else {
+                    store.push_back(curr);
+                    if (backtrack(num, i + 1, store, size + 1)) {
+                        return true;
+                    }
+                    store.pop_back();
+                }
+    
+                if (i == start && num[i] == '0') {
+                    break;
+                }
+            }
+            return false;
+        }
+    public:
+        bool isAdditiveNumber(string num) {
+            vector<long long> store;
+            return backtrack(num, 0, store, 0);
+        }
+    };
